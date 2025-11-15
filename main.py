9389b5d87cfb26664734a93459152cffa96daf67
vikornienko <vikornienko76@gmail.com>
2025-11-15 23:02:38 +0500

    Добавлены драйверы браузеров Хром и Эдж.

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from selenium.webdriver.firefox.service import Service
 service = Service(executable_path=".\\wdrs\\geckodriver.exe")
 options = webdriver.FirefoxOptions()
 
-driver = webdriver.Firefox(service=service, options=options)
+driver_firefox = webdriver.Firefox(service=service, options=options)
+driver_chrome = webdriver.Chrome()
+driver_edge = webdriver.Edge()
 
-driver.get("https://ya.ru")
+driver_firefox.get("https://ya.ru")
+driver_chrome.get("https://google.com")
+driver_edge.get("https://bing.com")
 
-print(driver.title)
-driver.quit()
+print(driver_firefox.title)
+print(driver_chrome.title)
+print(driver_edge.title)
+
+driver_firefox.quit()
+driver_chrome.quit()
+driver_edge.quit()
